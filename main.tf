5c9f61793550d4248e9e06f355a54d70d9f794c8
priti-777 <122448226+priti-777@users.noreply.github.com>
2025-03-04 12:47:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+resource "google_storage_bucket" "my-bucket" {
+  name          = "github"
   location      = "US"
   Project       ="tt-dev-01"
   force_destroy = true
