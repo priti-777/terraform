d5c2d578930297f1ad5dc73e924db69e0b58f0f7
priti-777 <122448226+priti-777@users.noreply.github.com>
2025-03-04 12:46:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = "auto-expiring-bucket"
   location      = "US"
+  Project       ="tt-dev-01"
   force_destroy = true
 
   lifecycle_rule {
